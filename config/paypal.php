c630437fbc8bbf9dbdc176cb3d24db7916e965a0
Trần Thiện Thanh <kurotsmile@gmail.com>
2026-06-18 16:56:22 +0700

    Add seo

--- config/paypal.php
+++ config/paypal.php
@@ -1,9 +1,9 @@
 <?php
 return [
     'enabled' => true,
-    'sandbox' => true,
-    'client_id' => getenv('PAYPAL_CLIENT_ID') ?: '',
-    'client_secret' => getenv('PAYPAL_CLIENT_SECRET') ?: '',
+    'sandbox' => false,
+    'client_id' => 'AU3B5PrpmGaSYi7beS7oBT3v7MhjIvsDD4O-psKj_qTVMc81tGpAw4O5vVB1b-zDoKF_t87V-AU7Ukrl',
+    'client_secret' => 'EIPtROoqxGxtQWd6ypx5ZDi1aXz9Zy3ya399O64Hfioi40ecnawdY80uLZsDTjmdXu04LDQOwon2Dfbk',
     'currency' => 'USD',
     'amount' => '5.00',
 ];
